39892e0078b4d8f8c12f9be6b147e0033f426e86
Clemens Fiedler <clemens.mb.fiedler@gmail.com>
2019-09-13 13:08:10 +0200

    minor bugfix

--- run.py
+++ run.py
@@ -13,25 +13,25 @@ vargs = vars(args)
 
 try:
     refresh = 60*int(vargs['refresh'])
-except:
+except None:
     refresh = 60*10
 
 try:
     time_end = 3600*int(vargs['time_end'])
-except:
+except None:
     time_end = 3600*24*30
 
 # start service
 display = InfoScreen()
 
-print('starting service\n' + '-'*20)
+print('starting service\n' + '-'*16)
 print('refresh: {} minutes'.format(refresh//60))
 
-if time_end<0:
-    print('runs indefinetly')
-if time_end>3600*2:
+if time_end < 0:
+    print('runs indefinitely')
+elif time_end>3600*2:
     print('stops : {} days'.format(time_end//(3600*24)))
 else:
-    print('stops : {} hours'.format(time_end //3600))
+    print('stops : {} hours'.format(time_end//3600))
 
 display.start_service(refresh=refresh, time_end=time_end)
